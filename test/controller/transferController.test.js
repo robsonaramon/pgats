ccee43bc9a36f23ba10fe4a472118919906596d4
Robson <robson.arakawa@gmail.com>
2025-08-09 16:08:44 -0300

    Adição do primeiro teste para validar o controller

---
+++ test/controller/transferController.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const sinon = require('sinon');
+const {expect} = require('chai');
+
+const app = require('../../app');
+
+describe('Transfer Controller', ()=> {
+    describe('POST /transfer', ()=> {
+        it ('Retornar o código 400 quando o remetente e destinatário são inexistentes', async ()=> {
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    remetente: "Marcos",
+                    destinatario: "Jones",
+                    valor: 1000
+                });
+
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body).to.have.property('error','Usuário não encontrado')
+        });
+    });
+});
