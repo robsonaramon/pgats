97aa95c5c2ed4f27f579088a9916d792390d2389
Robson <robson.arakawa@gmail.com>
2025-08-22 17:43:07 -0300

    Alteração no package.json

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -3,6 +3,7 @@ const sinon = require('sinon');
 const {expect} = require('chai');
 
 const app = require('../../app');
+const { transfer } = require('../../service/transferService');
 
 describe('Transfer Controller', ()=> {
     describe('POST /transfer', ()=> {
@@ -18,5 +19,10 @@ describe('Transfer Controller', ()=> {
             expect(resposta.status).to.equal(400);
             expect(resposta.body).to.have.property('error','Usuário não encontrado')
         });
+    });
+
+    it('Usando Mock: Retornar o código 400 quando o remetente e destinatário são inexistentes', async ()=> {
+        const transferServiceMock = sinon.stub(transferService, 'transfer');
+        transferServiceMock.transfer
     });
 });
