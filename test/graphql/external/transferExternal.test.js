e5db234b2d47864a2f6cf1498754ff5d1fc9d6c5
Robson <robson.arakawa@gmail.com>
2025-09-20 22:09:15 -0300

    Adicionando Data Driven Testing

--- test/graphql/external/transferExternal.test.js
+++ test/graphql/external/transferExternal.test.js
@@ -2,15 +2,15 @@ const request = require('supertest');
 const { expect, use } = require('chai');
 
 const chaiExclude = require('chai-exclude');
+const { transfer } = require('../../../service/transferService');
 use(chaiExclude);
-
-const url = 'http://localhost:4000/graphql'
+require('dotenv').config();
 
 describe('Teste de transferência', () => {
 
     before(async () => {
         const login = require('../fixture/request/login/login.json');
-        const resposta = await request(url)
+        const resposta = await request(process.env.BASE_URL_GRAPHQL)
             .post('')
             .send(login);
 
@@ -19,31 +19,31 @@ describe('Teste de transferência', () => {
 
 
     beforeEach(()=>{
-        transfer = require('../fixture/request/transfer/transfer.json');
+        fixtransfer = require('../fixture/request/transfer/transfer.json');
     })
 
     it('Validar tranferência entre duas contas', async ()=> {
         const respostaEsperada = require('../fixture/response/transfer/validarTranferênciaEntreDuasContas.json')
-        const respostaTransferencia = await request(url)
+        const respostaTransferencia = await request(process.env.BASE_URL_GRAPHQL)
             .post('')
             .set('Authorization', `Bearer ${token}`)
-            .send(transfer);
+            .send(fixtransfer);
 
         expect(respostaTransferencia.status).to.equal(200);
         expect(respostaTransferencia.body.data.transfer).excluding('data').to.eql(respostaEsperada.data.transfer);
     });
 
-    it('Validar que não é possível tranferir de uma conta com saldo insuficiente', async ()=> {
-        transfer.variables.input.valor = 10000;
+    const testeDeErrosDeNegocio = require('../fixture/request/transfer/tramsferWithError.json')
+    testeDeErrosDeNegocio.forEach(teste=> {
+        it(`Validar a tranferência com ${teste.nomeTeste}`, async ()=> {
+            const respostaTransferencia = await request(process.env.BASE_URL_GRAPHQL)
+                .post('')
+                .set('Authorization', `Bearer ${token}`)
+                .send(teste.transfer);
 
-        const respostaTransferencia = await request(url)
-            .post('')
-            .set('Authorization', `Bearer ${token}`)
-            .send(transfer);
-
-        expect(respostaTransferencia.status).to.equal(200);
-        expect(respostaTransferencia.body.errors[0].message).to.equal('Saldo insuficiente');
+            expect(respostaTransferencia.status).to.equal(200);
+            expect(respostaTransferencia.body.errors[0].message).to.equal(teste.mensagemEsperada);
+        });
     });
-
     
 });
